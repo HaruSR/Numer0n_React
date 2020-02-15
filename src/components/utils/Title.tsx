ac2838504c67ba2bf9c3fbd13a52ad39378ce742
Haruki_Ishikawa <haruki.ishikawa@jbs.com>
2020-02-15 15:15:50 +0900

    [update]change directory name[share=>utils]

---
+++ src/components/utils/Title.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Title = () => {
+    return (
+        <div className="container">
+            <div>N</div>
+            <div>u</div>
+            <div>m</div>
+            <div>e</div>
+            <div>r</div>
+            <div>0</div>
+            <div>n</div>
+        </div>
+    )
+}
+
+export default Title
